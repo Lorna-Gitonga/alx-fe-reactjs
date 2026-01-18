aa18d231fe4249e2d684b8858a836f625f046819
lorna <lornamuringi@gmail.com>
2026-01-18 21:02:52 +0300

    Fix App.jsx imports and render Header, MainContent, Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <WelcomeMessage />
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          Learn Vite
-        </a>
-      </div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-
-  
