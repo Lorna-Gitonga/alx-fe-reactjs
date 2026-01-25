9e556415dc4144d7ac641a4839d7ce43d1e8aa6b
lorna <lornamuringi@gmail.com>
2026-01-25 20:55:06 +0300

    Edit UserProfilepage

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,18 @@
-function UserProfile({ user }) {
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const user = useContext(UserContext); //consume context directly
+
   return (
     <div style={{
-      border: '1px solid #f1f19c',
+      border: '1px solid #ccc',
       padding: '15px',
       borderRadius: '10px',
       margin: '10px auto',
       width: '300px',
-      backgroundColor: '#db7864'
+      backgroundColor: '#f5f5f5',
+      textAlign: 'center'
     }}>
       <h3>{user.name}</h3>
       <p>{user.email}</p>
