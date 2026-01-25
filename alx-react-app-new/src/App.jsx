ea8fb4315c9ea5798061315ed2b035aac1c587c0
lorna <lornamuringi@gmail.com>
2026-01-25 19:19:02 +0300

    Added a counter Applocation

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,17 +2,25 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './Counter';
+
 
 function App() {
   return (
     <>
       <Header />
+
       <UserProfile
         name="Jane Doe"
         age={25}
         bio="A passionate traveler and React learner."
       />
+
       <MainContent />
+
+      <h1>My Counter App</h1>
+      <Counter />
+
       <Footer />
     </>
   );
