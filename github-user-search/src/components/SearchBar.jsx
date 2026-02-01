331fd229f5a81e36f985bdb96fd89d7f6354e25e
lorna <lornamuringi@gmail.com>
2026-02-01 13:35:38 +0300

    Add components directory and a search bar

--- github-user-search/src/components/SearchBar.jsx
+++ github-user-search/src/components/SearchBar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
       <p>Search for GitHub profiles using the GitHub API.</p>
+      <SearchBar />
     </div>
   );
 }
