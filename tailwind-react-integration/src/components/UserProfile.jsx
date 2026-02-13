7226e50084b4f7e229541369948a6b1b629f6be8
lorna <lornamuringi@gmail.com>
2026-02-13 13:34:47 +0300

    Added styling to image

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
          className="w-36 h-36 rounded-full mx-auto"
 
       />
-      <h1 className="text-xl text-blue-8800 my-4 font-semibold">
+      <h1 className="text-xl text-blue-800 my-4 font-semibold">
         John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
