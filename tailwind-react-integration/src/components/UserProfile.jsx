5fb290931dc89b3455bccb3990efb34a5a748270
lorna <lornamuringi@gmail.com>
2026-02-13 13:53:57 +0300

    Added sm to indicate sizing on small screens

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img src="https://via.placeholder.com/150" 
       alt="User" 
-         className="w-24 h-24 md:w-39 md:h-36 rounded-full mx-auto"
+         className="sm:w-24 sm:h-24 md:w-39 md:h-36 rounded-full mx-auto"
 
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 font-semibold">
