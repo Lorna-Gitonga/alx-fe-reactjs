e3474a7dcc91c76e448096064eb6e58e2b8fc881
lorna <lornamuringi@gmail.com>
2026-02-13 13:58:22 +0300

    Added text-sm in className

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
          className="sm:w-24 sm:h-24 md:w-39 md:h-36 rounded-full mx-auto"
 
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 font-semibold">
+      <h1 className="text-lg text-sm md:text-xl text-blue-800 my-4 font-semibold">
         John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
