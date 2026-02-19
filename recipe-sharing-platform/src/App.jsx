9f020571da98ffb51aee114fbdaf7355bdea36d4
lorna <lornamuringi@gmail.com>
2026-02-19 17:24:57 +0300

    Modification to ensur Tailwind Css configuration

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,9 +5,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind is Da bomb🚀
+       Recipe Sharing Platform 
       </h1>
     </div>
   )
