e3fec25f56c6ea683a3043a618fb521e3697bef7
lorna <lornamuringi@gmail.com>
2026-02-19 18:12:40 +0300

    Initial commit for the addition of the Home Page

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-blue-500">
-       Recipe Sharing Platform 
-      </h1>
-    </div>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
