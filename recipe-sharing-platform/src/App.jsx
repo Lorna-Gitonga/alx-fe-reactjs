1fdc70f8389ec4f180a1a54f8466e23dae48f088
lorna <lornamuringi@gmail.com>
2026-02-20 10:18:42 +0300

    Changed and corrected AddRecipeForm.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
-import AddRecipeForm from "./components/AddRecipeFrom";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
